88ab7f3d89b2c3154dafaef253ac66a3272ba4c5
mrutunjay50 <pmrutunjay928@gmail.com>
2025-12-15 00:02:21 +0530

    fixed too large issue

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ cron.schedule("0 0 * * *", async () => {
 async function startServer() {
   app.use(express.json());
   app.use(helmet({ crossOriginResourcePolicy: false }));
-  app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ limit: "50mb", extended: true }));
+  app.use(express.json({ limit: "100mb" }));
+  app.use(express.urlencoded({ limit: "100mb", extended: true }));
   app.use(morgan("common"));
   app.use(cors());
 
